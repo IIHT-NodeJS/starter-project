7dcb899d9588a6b64c3343efb5d8520755af3840
ngactive99 <ngactive99@gmail.com>
2022-04-26 11:04:28 +0530

    added git ignore and read me files

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 var http = require("http");
 var uc = require("upper-case");
-var nodeMailer = require("nodemailer");
-var transport = nodeMailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  auth: {
-    user: "ng.test88@gmail.com",
-    pass: "Admin123%",
-  },
-});
 
-var mailOptions = {
-  from: "ng.test88@gmail.com",
-  to: "ng.test88@gmail.com",
-  subject: "test node mail",
-  text: " this is the second mail that i am sending via node js",
-};
-transport.sendMail(mailOptions, function (error, info) {
-  if (error) {
-    console.warn(error);
-  } else {
-    console.warn("email has been sent", info.response);
-  }
-});
+
+
+// var nodeMailer = require("nodemailer");
+// var transport = nodeMailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false,
+//   requireTLS: true,
+//   auth: {
+//     user: "ng.test88@gmail.com",
+//     pass: "Admin123%",
+//   },
+// });
+
+// var mailOptions = {
+//   from: "ng.test88@gmail.com",
+//   to: "ng.test88@gmail.com",
+//   subject: "test node mail",
+//   text: " this is the second mail that i am sending via node js",
+// };
+// transport.sendMail(mailOptions, function (error, info) {
+//   if (error) {
+//     console.warn(error);
+//   } else {
+//     console.warn("email has been sent", info.response);
+//   }
+// });
 // var page = ` <div> <h1>Registration form</h1>
 // <br/>
 // <input type='text' placeholder='enter your name'/>
@@ -35,6 +38,14 @@ transport.sendMail(mailOptions, function (error, info) {
 //  </div>`
 
 // http.createServer(function(req , res) {
-//     res.write(uc.upperCase(" welcome to node web page"));
+//     res.write(uc.upperCase(" welcome to node js web page, we are learning about nodemon"));
 //     res.end();
 // }).listen(4000)
+
+var fs = require('fs');
+// to read a file
+var readStore= fs.readFileSync('readme.txt', 'utf8');
+// to write a file 
+var greet = "Good morning"
+fs.writeFileSync('writeStore.txt', greet );
+console.log(readStore);
--- readme.txt
+++ readme.txt
@@ -1,1 +1,13 @@
+steps to send mail
+
 step 1 - to install nodemailer (npm package)
+step 2 - to configure the email id and turn off secure apps
+
+steps to add nodemon 
+install nodemon 
+mention server is nodemon index.js 
+now ruin using npm run server
+
+steps to read and write a file
+1. to import fs into the program
+
